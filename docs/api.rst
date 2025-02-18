e2ddcfb27ab28d420ea5584ae794dab99561ae9a
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2025-02-17 23:58:56 -0500

    extensions for sphinx

--- docs/api.rst
+++ docs/api.rst
@@ -1,5 +1,11 @@
 API Reference
 =============
 
+Include the content here
+
 .. automodule:: agrtool.cleanupfiles
    :members:
+
+.. automodule:: agrtool.combine_results
+   :members:
+   :undoc-members:
--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,12 @@ import sys
 sys.path.insert(0, os.path.abspath('../src'))
 
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',  # Automatically generate docstrings
+    'sphinx.ext.napoleon',  # Support for Google style docstrings
+    'sphinx.ext.viewcode',  # Add links to source code
+    'sphinx_rtd_theme',     # Read the Docs theme
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -28,5 +33,23 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'alabaster' # 'sphinx_rtd_theme' # 'alabaster'
 html_static_path = ['_static']
+
+# If you want to add any custom styles, uncomment the following:
+# html_static_path = ['_static']
+
+# -- Options for autodoc -----------------------------------------------------
+
+# Automatically document members (functions, classes) and modules
+autodoc_default_flags = ['members', 'undoc-members', 'show-inheritance']
+
+# -- Options for Napoleon ----------------------------------------------------
+
+# Napoleon settings for Google-style docstrings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+
+# -- Additional settings -----------------------------------------------------
+
+# If you want to add more custom configurations for your documentation, you can do it here.
